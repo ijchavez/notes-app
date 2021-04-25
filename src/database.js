db519dfa89d37e337f4825b045a694fadb912765
Gerardo <info.gerardo.chavez@gmail.com>
2021-04-25 17:59:53 -0300

    database.js
    modificacion para integrar la app con heroku y mogodb

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //const NOTES_APP_MONGODB_DATABASE = process.env.NOTES_APP_MONGODB_DATABASE
 //suplanto lo de arriba por lo de abajo
 const {NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
-const MONGODB_URI= `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`
+const MONGODB_URI= `mongodb+srv://admin:Zz.1121224421@cluster0.haqjg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,

dd5554c5ed515e478ebe8a9f22fede86442d7eb4
this.gerardochavez <info.gerardo.chavez@gmail.com>
2024-08-11 13:31:30 -0300

    wip deployment in vercel

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const app = require("./server");
 require("./config/database");
 
 listen(app);
-//- en new notes estan los errores comentados SCRUM-13
-//- en edit notes esta comentado la redireccion a / en vez de /notes SCRUM-35
+/**
+ * en https://blue-darkness-3197.fly.dev/
+ *     estan los errores duplicados SCRUM-13
+ *     edit notes esta comentado la redireccion a / en vez de /notes SCRUM-35
+ *     edit notes al salvar no hace nada
+ *     logout no funciona
+ *     404 no muestra mensaje intuitivo
+ * */
+//- en https://blue-darkness-3197.fly.dev/ estan los errores comentados SCRUM-13
+//- en https://blue-darkness-3197.fly.dev/ edit notes esta comentado la redireccion a / en vez de /notes SCRUM-35
 //- en https://new-notes-app.fly.dev/ agregas una nota vacia y estalla SCRUM-13
